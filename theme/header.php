b8ef39589f4edbd3b7906f47cdb91c5dc0465b9e
Mike Bishop <mike@mniniman-webdesign.co.uk>
2025-10-22 18:32:10 +0100

    Scaffolding Header

--- theme/header.php
+++ theme/header.php
@@ -9,3 +9,29 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>> 
+    <header class="ccc26_global-header">
+        <div class="ccc26_wrap">
+            <div class="ccc26_global-header-column">
+                Logo
+            </div>
+            <div class="ccc26_global-header-column">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"/></svg>
+                                Your Favourites
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">Accessibility</a>
+                        </li>
+                        <li>
+                            <a href="#">Sustainability</a>
+                        </li>
+                        <li><a href="#">Search</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
