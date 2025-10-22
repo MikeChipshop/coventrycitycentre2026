306ef5fe6cd7cc746300365849d861e02335bdcd
Mike Bishop <mike@mniniman-webdesign.co.uk>
2025-10-22 17:59:19 +0100

    Init fonts

--- theme/functions.php
+++ theme/functions.php
@@ -30,18 +30,13 @@ ENQUEUES
 function dot_load_scripts() {
 
     wp_register_script( 'site-common', get_template_directory_uri() . '/js/site-common.js', array('jquery'),'',true  );
-	wp_register_script( 'splide', get_template_directory_uri() . '/js/splide.min.js', '','',true  );
-
 	wp_enqueue_script( 'site-common' );
-	wp_enqueue_script( 'splide' );
 
 	wp_register_style( 'main-css', get_template_directory_uri() . '/style.css?v=1','','', 'screen' );
-	wp_register_style( 'splide-css', get_template_directory_uri() . '/css/splide.min.css','','', 'screen' );
-	wp_register_style( 'gibson-css', 'https://use.typekit.net/uut7zfc.css','','', 'screen' );
+	wp_register_style( 'font-proxima-nova', 'https://use.typekit.net/fgj5ohh.css','','', 'screen' );
 	
-	wp_enqueue_style( 'main-css' );
-	wp_enqueue_style( 'splide-css' );	
-	wp_enqueue_style( 'gibson-css' );
+	wp_enqueue_style( 'main-css' );	
+	wp_enqueue_style( 'font-proxima-nova' );
 
 }
 
@@ -49,10 +44,7 @@ add_action('wp_enqueue_scripts', 'dot_load_scripts');
 
 function dot_block_styles() {
 	wp_enqueue_style( 'acf-font', get_template_directory_uri() . '/css/acf-styles.css');
-	wp_enqueue_style( 'gibson-font', 'https://use.typekit.net/uut7zfc.cssp');
-	wp_enqueue_style( 'splide-css', get_template_directory_uri() . '/css/splide.min.css');
-	wp_register_script( 'splide', get_template_directory_uri() . '/js/splide.min.js', '','',true  );
-	wp_enqueue_script( 'splide' );
+	wp_enqueue_style( 'font-proxima-nova', 'https://use.typekit.net/fgj5ohh.css');
 }
 add_action( 'enqueue_block_editor_assets', 'dot_block_styles' );
 
