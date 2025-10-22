96e3d21f5fb08483d908fb00a9a986866bc013af
Mike Bishop <mike@mniniman-webdesign.co.uk>
2025-10-22 18:17:52 +0100

    Scaffolding Footer

--- theme/footer.php
+++ theme/footer.php
@@ -1,3 +1,40 @@
+<footer class="ccc26_global-footer">
+    <div class="ccc26_wrap">
+        <div class="ccc26_global-footer-top">
+            <div class="ccc26_global-footer-column">
+                <nav>
+                    <h2>Helpful Links for Visitors</h2>
+                    <ul>
+                        <li><a href="#">Visit Coventry</a></li>
+                        <li><a href="#">Experience</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Dine</a></li>
+                        <li><a href="#">Travel</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="ccc26_global-footer-column">
+                <nav>
+                    <h2>Helpful Links for Business</h2>
+                    <ul>
+                        <li><a href="#">Conference Coventry</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Directory</a></li>
+                        <li><a href="#">Get Listed</a></li>
+                        <li><a href="#">The BID</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="ccc26_global-footer-column">
+                Logo
+            </div>
+        </div>
+        <div class="ccc26_global-footer-bottom">
+            <p>Site content copyright &copy;<?php echo date("Y"); ?></p>
+        </div>
+    </div>
+</footer>
 <?php wp_footer(); ?>        
 </body>
 </html>
