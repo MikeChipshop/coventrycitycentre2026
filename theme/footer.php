af7f738dc380160285e60b4301a27dfa58802206
Mike Bishop <mike@miniman-webdesign.co.uk>
2026-03-17 15:28:07 +0000

    Logo section for footer

--- theme/footer.php
+++ theme/footer.php
@@ -7,9 +7,21 @@
                 </nav>
             </div>
             <div class="ccc26_global-footer-column logo">
-                <a href="https://coventrybid.co.uk" target="_blank">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/img/coventrybid-logo.png" alt="Visit Coventry BID">
-                </a>
+                <?php if( have_rows('footer_logos','option') ): ?>
+                    <?php while( have_rows('footer_logos','option') ) : the_row(); ?>
+                        <?php
+                            $attachment_id = get_sub_field('footer_logos_image');
+                            $size = "full";
+                            $image = wp_get_attachment_image_src( $attachment_id, $size ); 
+
+                            $link = get_sub_field('footer_logos_link');
+                            $target = get_sub_field('footer_logos_link_target');
+                        ?>
+                        <a href="<?php echo $link; ?>"<?php if( $target ): ?> target="_blank"<?php endif; ?>>
+                            <img src="<?php echo $image[0]; ?>" alt="">
+                        </a>
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
         </div>
         <div class="ccc26_global-footer-bottom">
