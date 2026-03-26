a42565b567fb6d65b7a305d8da38a67f79d10583
Mike Bishop <mike@miniman-webdesign.co.uk>
2026-03-26 22:42:22 +0000

    Adding Flodesk form

--- theme/inc/blocks/pos/pos.php
+++ theme/inc/blocks/pos/pos.php
@@ -67,8 +67,82 @@
                 <?php echo $b2Content; ?>
 
                 <?php if($b2Form): ?>
+                    <script>
+                            (function(w, d, t, h, s, n) {
+                                w.FlodeskObject = n;
+                                var fn = function() {
+                                (w[n].q = w[n].q || []).push(arguments);
+                                };
+                                w[n] = w[n] || fn;
+                                var f = d.getElementsByTagName(t)[0];
+                                var v = '?v=' + Math.floor(new Date().getTime() / (120 * 1000)) * 60;
+                                var sm = d.createElement(t);
+                                sm.async = true;
+                                sm.type = 'module';
+                                sm.src = h + s + '.mjs' + v;
+                                f.parentNode.insertBefore(sm, f);
+                                var sn = d.createElement(t);
+                                sn.async = true;
+                                sn.noModule = true;
+                                sn.src = h + s + '.js' + v;
+                                f.parentNode.insertBefore(sn, f);
+                            })(window, document, 'script', 'https://assets.flodesk.com', '/universal', 'fd');
+                        </script>
+                        <script>
+                            window.fd('form:handle', {
+                                formId: '69c53b986c0e12d1f419b390',
+                                rootEl: '.ff-69c53b986c0e12d1f419b390',
+                            });
+                        </script>
                     <div class="ccc26_pos-form">
-                        <?php echo do_shortcode($b2Form); ?>
+                        <?php //echo do_shortcode($b2Form); ?>
+                        <div class="ff-69c53b986c0e12d1f419b390" data-ff-el="root" data-ff-version="3" data-ff-type="inline" data-ff-name="inlineNoImage" data-ff-stage="default">
+                            <!--tpl {% block config %} tpl-->
+                            <div data-ff-el="config" data-ff-config="eyJ0cmlnZ2VyIjp7Im1vZGUiOiJpbW1lZGlhdGVseSIsInZhbHVlIjowfSwib25TdWNjZXNzIjp7Im1vZGUiOiJtZXNzYWdlIiwibWVzc2FnZSI6IiIsInJlZGlyZWN0VXJsIjoiIn0sImNvaSI6ZmFsc2UsInNob3dGb3JSZXR1cm5WaXNpdG9ycyI6dHJ1ZSwibm90aWZpY2F0aW9uIjp0cnVlLCJnZHByIjp7ImFjY2VwdHNNYXJrZXRpbmciOmZhbHNlLCJwcml2YWN5UG9saWN5Ijp7ImVuYWJsZWQiOmZhbHNlLCJtYW5kYXRvcnkiOmZhbHNlfX0sInRyYWNraW5nQ29uZmlnIjp7Im1ldGFQaXhlbElkIjoiIiwiY29va2llQmFubmVyRW5hYmxlZCI6ZmFsc2UsImdvb2dsZUFuYWx5dGljc0lkIjoiIn19" style="display: none"></div>
+                            <!--tpl {% endblock %} tpl-->
+                            <div class="ff-69c53b986c0e12d1f419b390__container">
+                                <div class="ff-69c53b986c0e12d1f419b390__wrapper">
+                                    <form class="ff-69c53b986c0e12d1f419b390__form" action="https://form.flodesk.com/forms/69c53b986c0e12d1f419b390/submit" method="post" data-ff-el="form">
+                                        <div class="ff-69c53b986c0e12d1f419b390__content fd-form-content" data-ff-el="content">
+                                            <div class="ff-69c53b986c0e12d1f419b390__fields" data-ff-el="fields">
+                                                <!--tpl {% block fields %} tpl-->
+                                                <div class="ff-69c53b986c0e12d1f419b390__field fd-form-group">
+                                                    <input id="ff-69c53b986c0e12d1f419b390-email" class="ff-69c53b986c0e12d1f419b390__control fd-form-control" type="text" maxlength="255" name="email" placeholder="Email address" data-ff-tab="email::submit" required />
+                                                    <label for="ff-69c53b986c0e12d1f419b390-email" class="ff-69c53b986c0e12d1f419b390__label fd-form-label">
+                                                        <div>
+                                                            <div>Email address</div>
+                                                        </div>
+                                                    </label>
+                                                </div>
+                                                <input type="text" maxlength="255" name="confirm_email_address" style="display: none" />
+                                                <!--tpl {% endblock %} tpl-->
+                                            </div>
+
+                                            <div class="ff-69c53b986c0e12d1f419b390__footer" data-ff-el="footer">
+                                                <button type="submit" class="ff-69c53b986c0e12d1f419b390__button fd-btn" data-ff-el="submit" data-ff-tab="submit">
+                                                    <div><span data-draw-element="editable">Subscribe</span></div>
+                                                </button>
+                                            </div>
+
+                                            <!--tpl {% if var.Config.Gdpr.PrivacyPolicy.Enabled and var.Config.Gdpr.AcceptsMarketing == false and var.Config.Gdpr.PrivacyPolicy.Mandatory == false %} tpl-->
+
+                                            <!--tpl {% endif %} tpl-->
+
+                                        </div>
+                                        <div class="ff-69c53b986c0e12d1f419b390__success fd-form-success" data-ff-el="success">
+                                            <div class="ff-69c53b986c0e12d1f419b390__success-message">
+                                                <div>
+                                                    <div>
+                                                        <div data-paragraph="true">Thank you for subscribing!</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="ff-69c53b986c0e12d1f419b390__error fd-form-error" data-ff-el="error"></div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>                        
                     </div>
                 <?php endif; ?>
             </div>            
