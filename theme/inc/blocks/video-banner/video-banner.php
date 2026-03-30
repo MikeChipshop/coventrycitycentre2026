3fc9660f8f2fe2311ec867b12536fe66eef754b8
Mike Bishop <mike@miniman-webdesign.co.uk>
2026-03-30 12:04:33 +0100

    Added mute button to video

--- theme/inc/blocks/video-banner/video-banner.php
+++ theme/inc/blocks/video-banner/video-banner.php
@@ -27,10 +27,36 @@
             </div>
         <?php else: ?>
             <div class="ccc26_video-embed">
-            <video autoplay muted loop>
+            <button id="mute-<?php echo esc_attr($id); ?>">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                    <path d="M792-56 671-177q-25 16-53 27.5T560-131v-82q14-5 27.5-10t25.5-12L480-368v208L280-360H120v-240h128L56-792l56-56 736 736-56 56Zm-8-232-58-58q17-31 25.5-65t8.5-70q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 53-14.5 102T784-288ZM650-422l-90-90v-130q47 22 73.5 66t26.5 96q0 15-2.5 29.5T650-422ZM480-592 376-696l104-104v208Zm-80 238v-94l-72-72H200v80h114l86 86Zm-36-130Z"/>
+                </svg>
+            </button>
+            
+            <video id="video-<?php echo esc_attr($id); ?>" autoplay muted loop>
                 <source src="<?php echo get_field('uploaded_video'); ?>" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
+
+            <script>
+                const iconMute = `
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#e3e3e3">
+                <path d="M560-131v-82q90-26 145-100t55-168q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 127-78 224.5T560-131ZM120-360v-240h160l200-200v640L280-360H120Zm440 40v-322q47 22 73.5 66t26.5 96q0 51-26.5 94.5T560-320ZM400-606l-86 86H200v80h114l86 86v-252Z"/>
+                </svg>
+                `;
+
+                const iconUnmute = `
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M792-56 671-177q-25 16-53 27.5T560-131v-82q14-5 27.5-10t25.5-12L480-368v208L280-360H120v-240h128L56-792l56-56 736 736-56 56Zm-8-232-58-58q17-31 25.5-65t8.5-70q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 53-14.5 102T784-288ZM650-422l-90-90v-130q47 22 73.5 66t26.5 96q0 15-2.5 29.5T650-422ZM480-592 376-696l104-104v208Zm-80 238v-94l-72-72H200v80h114l86 86Zm-36-130Z"/></svg>
+                `;
+
+                const video = document.getElementById('video-<?php echo esc_attr($id); ?>');
+                const btn = document.getElementById('mute-<?php echo esc_attr($id); ?>');
+
+                btn.addEventListener('click', () => {
+                video.muted = !video.muted;
+                btn.innerHTML = video.muted ? iconUnmute : iconMute;
+                });
+            </script>
         </div>
         <?php endif; ?>
     </div>
